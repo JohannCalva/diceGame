bc691f7bdbfb637148b055abfa0785c32e16f3fb
JohannCalva <138340005+JohannCalva@users.noreply.github.com>
2025-02-24 19:21:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var imageSource1 = "./images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", imageSource1);
+
+var imageSource2 = "./images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", imageSource2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🤩Player 1 Wins!";
+}else if (randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw";
+}else{
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🤩";
+}
